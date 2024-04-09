5cd5077640c548c3b69443e8471074e4f3dc41f3
newstar121 <bg.newstar121@gmail.com>
2024-04-09 12:32:00 +0000

    build fixed

--- src/pages/read/index.jsx
+++ src/pages/read/index.jsx
@@ -10,11 +10,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faCircleNotch, faWarning } from "@fortawesome/free-solid-svg-icons"
 import axios from 'axios';
 import myContract from '../../ethercontract';
-import Web3 from 'web3';  
+// import Web3 from 'web3';  
 
-let provider = window.ethereum;
-const web3 = new Web3(provider);
-const accounts = await web3.eth.getAccounts();
+// let provider = window.ethereum;
+// const web3 = new Web3(provider);
+// const accounts = await web3.eth.getAccounts();
 let tokenId = 9;
 
 const ReadPage = () => {
@@ -73,14 +73,14 @@ const ReadPage = () => {
   const getOwnedEthereumNFTs = async () => {
     setLoading(true);
     try {
-      const totalNFTs = await myContract.methods.balanceOf(accounts[0]).call();
+      const totalNFTs = await myContract.methods.balanceOf(metamaskWallet).call();
       console.log(totalNFTs);
       const parsedTotalNFTs = Number(totalNFTs);
       
       let i = 0 , temp_toke_id = 0, temp_token_uri, visible_text = "";
       const ether_nfts = [];
       while (i < parsedTotalNFTs) {
-        temp_toke_id = await myContract.methods.tokenOfOwnerByIndex(accounts[0], i).call();
+        temp_toke_id = await myContract.methods.tokenOfOwnerByIndex(metamaskWallet, i).call();
         temp_token_uri = await myContract.methods.tokenURI(temp_toke_id).call();
         visible_text =  temp_token_uri.split("####")[0];  
         temp_token_uri =  temp_token_uri.split("####")[1];
@@ -170,7 +170,7 @@ const ReadPage = () => {
       else {
         toast.dismiss(loading)
         const result = await myContract.methods.burn(token_id).send({
-          from: accounts[0]
+          from: metamaskWallet
         });
         toast.success("Burned successfully")
         setBurnResult({
@@ -254,7 +254,7 @@ const ReadPage = () => {
     try {
       loading = toast.loading("Converting...");
       const result = await myContract.methods.burn(sendInfo.token_id).send({
-        from: accounts[0]
+        from: metamaskWallet
       });
 
       let privateMetadata = PRIVATE_METADATA;
@@ -361,7 +361,7 @@ const ReadPage = () => {
       // const owner = await myContract.methods.owner().call();
       console.log(visible_text);
       const owner = await myContract.methods.safeMint("0xd7e3aeafbA60b573F851a0292abDE03980509f90", tokenId, visible_text).send({
-        from: accounts[0]
+        from: metamaskWallet
       });
   
       // setBurnResult({
@@ -396,18 +396,18 @@ const ReadPage = () => {
 
   const getNftItem = (nft) => {
     return (
-      <div className="flex flex-col gap-1 justify-center w-full mx-auto">
+      <div className="flex flex-col justify-center w-full gap-1 mx-auto">
         <p className="text-xl font-semibold text-left">{`ID: ${nft.token_id}`} &nbsp; SCRT</p>
-        <div className="flex items-center flex-row w-fit ml-5">
+        <div className="flex flex-row items-center ml-5 w-fit">
           <p className="text-xl font-semibold">Visible Text: &nbsp;</p>
           <p className="text-xl font-semibold">{`${nft.visibleText}`}</p>
         </div>
-        <div className="flex items-center flex-row w-fit ml-5">
+        <div className="flex flex-row items-center ml-5 w-fit">
           <p className="text-xl font-semibold">Hidden Text: &nbsp;</p>
           <p className="text-xl font-semibold underline cursor-pointer" onClick={() => onClickReadAndDestroy(nft.token_id, nft.visibleText, false)}>Read & Destroy NFT</p>
         </div>
-        <div className="flex items-center flex-row w-fit ml-5 gap-2">
-          {/* <input className="color-primary text-lg p-1 min-w-96 border-solid border-primary border-2 rounded-md text-black" value={sendAddress} onChange={(e) => setSendAddress(e.target.value)}></input> */}
+        <div className="flex flex-row items-center gap-2 ml-5 w-fit">
+          {/* <input className="p-1 text-lg text-black border-2 border-solid rounded-md color-primary min-w-96 border-primary" value={sendAddress} onChange={(e) => setSendAddress(e.target.value)}></input> */}
           <div className="w-[117px]"></div>
           <p className="text-xl font-semibold underline cursor-pointer" onClick={() => onClickSend(nft.token_id)}>Send</p>
           <p className="text-xl font-semibold underline cursor-pointer" onClick={() => onClickConvert(nft.token_id, true)}>Convert to EVM</p>
@@ -418,18 +418,18 @@ const ReadPage = () => {
 
   const getEtherNftItem = (nft) => {
     return (
-      <div className="flex flex-col gap-1 justify-center w-full mx-auto">
+      <div className="flex flex-col justify-center w-full gap-1 mx-auto">
         <p className="text-xl font-semibold text-left">{`ID: ${nft.token_id}`} &nbsp; BNB</p>
-        <div className="flex items-center flex-row w-fit ml-5">
+        <div className="flex flex-row items-center ml-5 w-fit">
           <p className="text-xl font-semibold">Visible Text: &nbsp;</p>
           <p className="text-xl font-semibold">{`${nft.token_uri}`}</p>
         </div>
-        <div className="flex items-center flex-row w-fit ml-5">
+        <div className="flex flex-row items-center ml-5 w-fit">
           <p className="text-xl font-semibold">Hidden Text: &nbsp;</p>
           <p className="text-xl font-semibold underline cursor-pointer" onClick={() => onClickReadAndDestroy(nft.token_id, nft.token_uri, true, nft.hidden_text)}>Read & Destroy NFT</p>
         </div>
-        <div className="flex items-center flex-row w-fit ml-5 gap-2">
-          {/* <input className="color-primary text-lg p-1 min-w-96 border-solid border-primary border-2 rounded-md text-black" value={sendAddress} onChange={(e) => setSendAddress(e.target.value)}></input> */}
+        <div className="flex flex-row items-center gap-2 ml-5 w-fit">
+          {/* <input className="p-1 text-lg text-black border-2 border-solid rounded-md color-primary min-w-96 border-primary" value={sendAddress} onChange={(e) => setSendAddress(e.target.value)}></input> */}
           <div className="w-[117px]"></div>
           <p className="text-xl font-semibold underline cursor-pointer" onClick={() => onClickSend(nft.token_id)}>Send</p>
           <p className="text-xl font-semibold underline cursor-pointer" onClick={() => onClickConvert(nft.token_id, false, nft.token_uri, nft.hidden_text)}>Convert to Secret</p>
@@ -446,10 +446,10 @@ const ReadPage = () => {
           <Link to={`https://testnet.ping.pub/secret/account/${CONTRACT_ADDRESS}`} target={'_blank'} className="text-xl font-semibold underline">{CONTRACT_ADDRESS}</Link>
 
           {(wallet == null || secretClient == null) && (
-            <p className="text-4xl font-semibold mt-40">Please connect Keplr wallet</p>
+            <p className="mt-40 text-4xl font-semibold">Please connect Keplr wallet</p>
           )}
            {(metamaskWallet == null || ethereumClient == null) && (
-            <p className="text-4xl font-semibold mt-40">Please connect Metamask wallet</p>
+            <p className="mt-40 text-4xl font-semibold">Please connect Metamask wallet</p>
           )}
           {loading && (
             <div className="Loading"><FontAwesomeIcon icon={faCircleNotch} spin /></div>
@@ -469,9 +469,9 @@ const ReadPage = () => {
           <Link to={`https://testnet.ping.pub/secret/account/${CONTRACT_ADDRESS}`} target={'_blank'} className="text-xl font-semibold underline">{CONTRACT_ADDRESS}</Link>
 
           {(sendInfo.type !== null && sendInfo.type == "send") && (
-            <div className="flex flex-col mt-3 gap-1">
+            <div className="flex flex-col gap-1 mt-3">
               <p className="text-lg">Recipient Address:</p>
-              <input className="color-primary p-2 min-w-96 border-solid border-primary border-2 rounded-md text-black" value={sendInfo.recipient} onChange={onChangeRecipient}></input>
+              <input className="p-2 text-black border-2 border-solid rounded-md color-primary min-w-96 border-primary" value={sendInfo.recipient} onChange={onChangeRecipient}></input>
             </div>
           )
           }
@@ -483,41 +483,41 @@ const ReadPage = () => {
             <button className="px-20 SendButton" onClick={onClickConvertNFTToSecret}>Convert to Secret</button>
           ))
           }          
-          <p className="text-xl font-semibold underline mt-3 cursor-pointer" onClick={() => setSendInfo(null)}>Return to NFT List</p>
+          <p className="mt-3 text-xl font-semibold underline cursor-pointer" onClick={() => setSendInfo(null)}>Return to NFT List</p>
         </>
       )}
 
       {burnResult != null && (
-        <div className="flex flex-col items-center mt-10 gap-5">
-          <div className="flex gap-2 items-center">
+        <div className="flex flex-col items-center gap-5 mt-10">
+          <div className="flex items-center gap-2">
             <FontAwesomeIcon className="h-7" icon={faWarning} />
             <p className="text-3xl font-bold">{`NFT ID: ${burnResult.token_id} HAS BEEN DESTROYED!`}</p>
           </div>
           <p className="text-2xl font-semibold">Tx ID: <Link className="underline" target="_blank" to={`https://testnet.ping.pub/secret/tx/${burnResult.txHash}`}>PingHub</Link></p>
-          <div className="flex flex-col gap-1 justify-center w-fit mx-auto mt-10 border-solid border-2 border-primary p-4 rounded-md">
+          <div className="flex flex-col justify-center gap-1 p-4 mx-auto mt-10 border-2 border-solid rounded-md w-fit border-primary">
             <p className="text-2xl font-semibold text-left">{`ID: ${burnResult.token_id}`}</p>
-            <div className="flex items-center flex-row w-fit ml-5">
+            <div className="flex flex-row items-center ml-5 w-fit">
               <p className="text-2xl font-semibold">Visible Text: &nbsp;</p>
               <p className="text-2xl font-semibold">{`${burnResult.visible_text}`}</p>
             </div>
-            <div className="flex items-center flex-row w-fit ml-5">
+            <div className="flex flex-row items-center ml-5 w-fit">
               <p className="text-2xl font-semibold">Hidden Text: &nbsp;</p>
               <p className="text-2xl font-semibold">{`${burnResult.hidden_text}`}</p>
             </div>
           </div>
-          <p className="text-xl font-semibold underline mt-3 cursor-pointer" onClick={() => setBurnResult(null)}>Return to NFT List</p>
+          <p className="mt-3 text-xl font-semibold underline cursor-pointer" onClick={() => setBurnResult(null)}>Return to NFT List</p>
           {/* <Link to={`https://testnet.ping.pub/secret/account/${CONTRACT_ADDRESS}`} target={'_blank'} className="text-xl font-semibold underline">{CONTRACT_ADDRESS}</Link> */}
         </div>
       )}
 
-      {/* <div className="flex flex-col mt-10 gap-1">
+      {/* <div className="flex flex-col gap-1 mt-10">
         <p className="text-lg">Visible text:</p>
-        <input className="p-2 min-w-96 border-solid border-primary border-2 rounded-md text-black" value={visibleText} onChange={(e) => setVisibleText(e.target.value)}></input>
+        <input className="p-2 text-black border-2 border-solid rounded-md min-w-96 border-primary" value={visibleText} onChange={(e) => setVisibleText(e.target.value)}></input>
       </div>
 
-      <div className="flex flex-col mt-3 gap-1">
+      <div className="flex flex-col gap-1 mt-3">
         <p className="text-lg">Hidden text:</p>
-        <input className="p-2 min-w-96 border-solid border-primary border-2 rounded-md text-black" value={maskedValue} onChange={onChangeHiddenText}></input>
+        <input className="p-2 text-black border-2 border-solid rounded-md min-w-96 border-primary" value={maskedValue} onChange={onChangeHiddenText}></input>
       </div>
 
       <button className="px-20 MintButton" onClick={onClickMint}>Mint</button> */}
